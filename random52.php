ea54e7824b216c4df1b26e4d839852ffb1c49942
IT5706021631069 <poozaza2006@hotmail.com>
2016-02-18 21:03:35 +0700

    work2 (update1)

--- random52.php
+++ random52.php
@@ -5,12 +5,18 @@
 <title>Untitled Document</title>
 </head>
 <body>
+  <table width="80%" align="center" border="1">
 <?php
    srand((double)microtime()*1000000);
-for ($i=1 ; $i <= 52;$i++){
+for ($i=1 ; $i <= 13 ;$i++){
+  echo "<tr>";
+  for ($c=1 ; $c <= 4 ;$c++){
    $v = rand(0,100);
-   echo $i ," : ",$v,"<br>";
-}
-?>
+   echo "<td><center> $v </center></td>" ;
+ }
+ echo " </tr> " ;
+ }
+ ?>
+</table>
 </body>
 </html>
